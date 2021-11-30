f9460be2a0519befd07eadff5358369395295ccf
kells <pashaglytov2004@mail.ru>
2021-11-30 15:55:28 +0300

    added eng comments

--- task5/laba_5.5/laba_5.5.cpp
+++ task5/laba_5.5/laba_5.5.cpp
@@ -16,68 +16,68 @@ int CorrentInput() {
 }
 
 void AddItem(int*& arr, int size, const int item) {
-    int* newArray = new int[size + 1];
+    int* newArray = new int[size + 1]; // creating a new dynamic array with size + 1 elements
     for (int i = 0; i < size; i++) {
-        newArray[i + 1] = arr[i];
+        newArray[i + 1] = arr[i]; // copying elements to a new array from the 1st position
     }
-    newArray[0] = item;
-    size++;
-    delete[] arr;
-    arr = newArray;
+    newArray[0] = item; // assigning the zero position of an array to a variable
+    size++; // increasing the size of the array by one unit
+    delete[] arr; // clearing heap of old array
+    arr = newArray; // substitution of pointers
 }
 
-void Averange(int** arr, const int size) {
-    int k = size * size;
+void Averange(int** arr, const int size) { // function for two-dimensional array
+    int k = size * size; // get the quantity
     double averange_value = 0;
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            averange_value += arr[i][j];
+            averange_value += arr[i][j]; // sum of all values
         }
     }
-    averange_value /= k;
+    averange_value /= k; // get the averange value
     cout << averange_value;
 }
 
-void Averange(int* arr, const int size) {
+void Averange(int* arr, const int size) { // function fo one-dimensional array
     int averange_value = 0;
     for (int i = 0; i < size; i++) {
-        averange_value += arr[i];
+        averange_value += arr[i]; // sum of all values
     }
-    averange_value /= size;
+    averange_value /= size; // get the averange value
     cout << averange_value;
 
 }
 
 int main() {
     srand(time(NULL));
-    int* B = new int[0];
+    int* B = new int[0]; // creating a pointer to a dynamic array of 0 elements 
     cout << "enter n\n";
     int n = CorrentInput();
-    int** A = new int* [n];
+    int** A = new int* [n]; // creating a one-dimensional array of pointers to one-dimensional arrays
     for (int i = 0; i < n; i++) {
-        A[i] = new int[n];
+        A[i] = new int[n]; // allocation of dynamic memory
     }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            A[i][j] = rand() % 10;
-            cout << A[i][j] << " ";
+            A[i][j] = rand() % 10; // random input of array
+            cout << A[i][j] << " "; // and output for checking
         }
         cout << "\n";
     }
     starline();
-    int q = 0;
+    int q = 0;  // counter
     for (int j = 1; j < n; j += 2) {
         for (int i = 0; i < n; i++) {
             int temp = 0;
             if (A[i][j] % 2) {
                 temp = A[i][j];
-                q++;
-                AddItem(B, q, temp);
+                q++; // increace value of counter
+                AddItem(B, q, temp);  // push such element into new array
             }
         }
     }
     for (int i = 0; i < q; i++) {
-        cout << B[i] << " ";
+        cout << B[i] << " "; // output of new arrat
     }
     starline();
     cout << "the averange value of big array : ";
@@ -86,6 +86,7 @@ int main() {
     cout << "the averange value of new array : ";
     Averange(B, q);
     cout << "\n\n";
+    // freeing heap
     for (int i = 0; i < n; i++) {
         delete[] A[i];
     }
