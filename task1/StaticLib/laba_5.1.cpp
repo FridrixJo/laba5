f141f8a16f27c5f5cc076e5efda9dc6c9e3e3538
kells <pashaglytov2004@mail.ru>
2021-11-30 15:21:46 +0300

    added eng comments

--- task1/StaticLib/laba_5.1.cpp
+++ task1/StaticLib/laba_5.1.cpp
@@ -1,12 +1,12 @@
 ﻿void AddItem(int*& arr, int size, const int item) {
-    int* newArray = new int[size + 1]; // создание нового динамического массива с size + 1 элементов
+    int* newArray = new int[size + 1]; // creating a new dynamic array with size + 1 elements
     for (int i = 0; i < size; i++) {
-        newArray[i + 1] = arr[i]; // копирование элементов в новый массив с 1-ой позиции
+        newArray[i + 1] = arr[i]; // copying elements to a new array from the 1st position
     }
-    newArray[0] = item; // присваивание нулевой позиции массива переменной 
-    size++; // увеличение размерности массива на одну единицу
-    delete[] arr; // чистка динамической памяти старого массива
-    arr = newArray; // подмена указателей
+    newArray[0] = item; // assigning the zero position of an array to a variable
+    size++; // increasing the size of the array by one unit
+    delete[] arr; // clearing heap of old array
+    arr = newArray; // substitution of pointers
 } 
 
 
--- task1/laba_5.1/main.cpp
+++ task1/laba_5.1/main.cpp
@@ -14,24 +14,24 @@ int CorrentInput() {
 
 int main()
     cout << "enter the value of n\n";
-    int n = CorrentInput();  // ввод n
+    int n = CorrentInput();  // input n
     cout << "enter the value of m\n";
-    int m = CorrentInput();  // ввод m
-    int* S = new int[m]; //создание указателся на динамический массив из m элементов 
-    int** A = new int* [n]; // создание одномерного массива указателей на одномерные массивы
+    int m = CorrentInput();  // input m
+    int* S = new int[m]; // creating a pointer to a dynamic array of m elements 
+    int** A = new int* [n]; // creating a one-dimensional array of pointers to one-dimensional arrays
 
     for (int i = 0; i < n; i++) {
-        A[i] = new int[m]; // выделение динамической памяти
+        A[i] = new int[m]; // allocation of dynamic memory
     }
     cout << "full fill your array\n";
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            A[i][j] = CorrentInput(); // ввод значений массива
+            A[i][j] = CorrentInput(); // input values of array
         }
     }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cout << A[i][j] << " "; // вывод массива для проверки правильности ввода
+            cout << A[i][j] << " "; // outputting an array to check if the input is correct
         }
         cout << "\n";
     }
@@ -40,16 +40,16 @@ int main()
         for (int i = 0; i < n; i++) {
             temp += A[i][j];
         }
-        AddItem(S, j, temp); // добавление переменной в новый динамический массив
+        AddItem(S, j, temp); // adding a variable to a new dynamic array
     }
     int min = INT_MAX;
-    for (int i = 0; i < m; i++) { // перебор нового динамического массива
+    for (int i = 0; i < m; i++) { // iteration over a new dynamic array
         if (S[i] < min) {
-            min = S[i]; // присваивание наименьшего элемента массива к переменной min
+            min = S[i]; // assigning the smallest element of an array to variable min
         }
     }
-    cout << min << "\n"; // вывод наименьшего значения
-    // освобождение динамической пямяти 
+    cout << min << "\n"; // output of smallest value
+    // freeing heap 
     for (int i = 0; i < n; i++) {
         delete[] A[i]; 
     }
